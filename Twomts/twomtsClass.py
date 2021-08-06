6c786310ab4f092e90cadbf754a8464879a5a34f
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-08-06 22:13:02 +0200

    Delete Twomts directory

--- Twomts/twomtsClass.py
+++
@@ -1,9 +0,0 @@
-class twomt():
-    def __init__(self, id, poster, content, date, replyTo, posterUsername):
-        self.id = id
-        self.poster = poster
-        self.replyTo = replyTo
-        self.content = content
-        self.date = date
-        self.posterUsername = posterUsername
-        self.replies = []
